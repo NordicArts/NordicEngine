c0fdec4c0e5a0e1aa9fabb8659be3ef80b8b38b2
Keloran <keloran@develbox.info>
2014-12-22 10:24:14 +0000

    moved stuff around

--- cleaner.sh
+++ cleaner.sh
@@ -8,6 +8,8 @@ rm -rf `find . -type d -name build`
 rm -rf `find . -type f -name CMakeCache.txt`
 rm -rf `find . -type f -name Makefile`
 rm -rf `find . -type f -name cmake_install.cmake`
+rm -rf `find . -type f -name CPackConfig.cmake`
+rm -rf `find . -type f -name CPackSourceConfig.cmake`
 
 # Delete old libraries
 rm -rf `find . -type f -name libGLFW.*`
