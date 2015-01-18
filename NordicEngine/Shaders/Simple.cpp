c6814fedffe6134b48c6dee2b803ba61f57f1fc1
Keloran <keloran@nordicarts.net>
2015-01-18 13:30:34 +0000

    updated subs

--- NordicEngine/Shaders/Simple.cpp
+++ NordicEngine/Shaders/Simple.cpp
@@ -1,5 +1,6 @@
 #include <NordicEngine/Shaders/Simple.hpp>
 #include <NordicEngine/Files/Files.hpp>
+#include <OpenGL/OpenGL.h>
 
 namespace NordicArts {
     namespace NordicEngine {
@@ -8,24 +9,61 @@ namespace NordicArts {
             }
 
             GLuint Simple::LoadShaders(std::string cVertex, std::string cFragment) {
-GLuint iRet;                
-
-//                GLuint iVertexID    = glCreateShader(GL_VERTEX_SHADER);
-//                GLuint iFragmentID  = glCreateShader(GL_FRAGMENT_SHADER);
+                GLuint iVertexID    = glCreateShader(GL_VERTEX_SHADER);
+                GLuint iFragmentID  = glCreateShader(GL_FRAGMENT_SHADER);
 
                 // get code
                 Files oVertex(cVertex);
                 std::string cVertexCode = oVertex.read();
 
-                printIt(cVertexCode);
-
                 Files oFragment(cFragment);
                 std::string cFragmentCode = oFragment.read();
-            
-                printIt(cFragmentCode);
 
-return iRet;    
-            }        
+                GLint iResult = GL_FALSE;
+                int iInfoLogLength;
+
+                // Vertex
+                char const *VertexSourcePointer = cVertexCode.c_str();
+                glShaderSource(iVertexID, 1, &VertexSourcePointer, NULL);
+                glCompileShader(iVertexID);
+
+                glGetShaderiv(iVertexID, GL_COMPILE_STATUS, &iResult);
+                glGetShaderiv(iVertexID, GL_INFO_LOG_LENGTH, &iInfoLogLength);
+                if (iInfoLogLength > 0) {
+                    std::vector<char> VertexShaderErrorMessage(iInfoLogLength + 1);
+                    glGetShaderInfoLog(iVertexID, iInfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+                }
+
+                // Fragment
+                char const *FragmentSourcePointer = cFragmentCode.c_str();
+                glShaderSource(iFragmentID, 1, &FragmentSourcePointer, NULL);
+                glCompileShader(iFragmentID);
+
+                glGetShaderiv(iFragmentID, GL_COMPILE_STATUS, &iResult);
+                glGetShaderiv(iFragmentID, GL_INFO_LOG_LENGTH, &iInfoLogLength);
+                if (iInfoLogLength > 0) {
+                    std::vector<char> FragmentShaderErrorMessage(iInfoLogLength + 1);
+                    glGetShaderInfoLog(iFragmentID, iInfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+                }
+
+                // Program
+                GLuint iProgramID = glCreateProgram();
+                glAttachShader(iProgramID, iVertexID);
+                glAttachShader(iProgramID, iFragmentID);
+                glLinkProgram(iProgramID);
+
+                glGetProgramiv(iProgramID, GL_LINK_STATUS, &iResult);
+                glGetProgramiv(iProgramID, GL_INFO_LOG_LENGTH, &iInfoLogLength);
+                if (iInfoLogLength > 0) {
+                    std::vector<char> ProgramShaderErrorMessage(iInfoLogLength + 1);
+                    glGetShaderInfoLog(iProgramID, iInfoLogLength, NULL, &ProgramShaderErrorMessage[0]);
+                }
+
+                glDeleteShader(iVertexID);
+                glDeleteShader(iFragmentID);
+
+                return iProgramID;
+            }
         };
     };
 };
