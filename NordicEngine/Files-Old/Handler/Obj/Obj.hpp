acec8fa57f7050e43081402945319575912fc4b0
Keloran <keloran@develbox.info>
2015-01-21 10:58:28 +0000

    moved old file handler out

---
+++ NordicEngine/Files-Old/Handler/Obj/Obj.hpp
@@ -0,0 +1,13 @@
+#ifndef NordicArts_NordicEngine_Files_Handler_OBJ_H
+#define NordicArts_NordicEngine_Files_Handler_OBJ_H
+
+#include <NordicEngine/Engine.hpp>
+#include <NordicEngine/Files/Handler/Handler.hpp>
+
+namespace NordicArts {
+    namespace NordicEngine {
+        
+    };
+};
+
+#endif
